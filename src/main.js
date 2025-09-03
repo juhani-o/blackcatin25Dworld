5c4bc852dd8f720d81672fe68fb4f4d7adb64230
Juhani Ovalo <juhani.ovalo@gmail.com>
2025-09-03 23:00:51 +0300

    Work with collision detection

--- src/main.js
+++ src/main.js
@@ -4,24 +4,71 @@ import noppa from "./assets/noppa2.png";
 const canvas = document.getElementById("myCanvas");
 const gl = canvas.getContext("webgl2");
 
-let z = 20;
+let z = 10;
 let rot = 0;
 let playerX = 8;
 let playerY = 15;
-let playerXvelocity = 0.01;
-let playerYvelocity = -0.05;
+let playerXvelocity = 0.0;
+let playerYvelocity = 0;
 
 let image = new Image();
 image.src = noppa;
 
 function checkCollision() {
-  let mapSize = map1.length;
-  let character = map1[Math.floor(mapSize - playerY + 1)][Math.round(playerX)];
-  console.log("Character ", character);
-  if (character === "#") {
-    playerYvelocity = 0;
+  let checkX = 0;
+  let checkY = 0;
+
+  // Check which direction to check collision
+  if (playerXvelocity > 0) {
+    checkX = 1;
+  } else if (playerXvelocity < 0) {
+    checkX = -1;
   }
+
+  if (playerYvelocity > 0) {
+    checkY = 1;
+  } else if (playerYvelocity < 0) {
+    checkY = -1;
+  }
+
+  // Let's test collision
+  let mapSize = map1.length;
+  let locationY = Math.round(mapSize - playerY);
+  let locationX = Math.round(playerX);
+  let test = Array(3);
+  test[0] = map1[locationY + 1].slice(locationX - 1, locationX + 2);
+  test[1] = map1[locationY].slice(locationX - 1, locationX + 2);
+  test[2] = map1[locationY - 1].slice(locationX - 1, locationX + 2);
+  console.log("test ", test);
 }
+
+document.addEventListener("keydown", (event) => {
+  const keyName = event.key.toLowerCase();
+
+  switch (keyName) {
+    case "w":
+      playerYvelocity = 0.01;
+      break;
+    case "a":
+      playerXvelocity = -0.01;
+      break;
+    case "s":
+      playerYvelocity = -0.01;
+      break;
+    case "d":
+      playerXvelocity = 0.01;
+      break;
+    default:
+      // Optional: You can handle other keys here if needed
+      // console.log(`Key pressed: ${keyName}`);
+      break;
+  }
+});
+
+document.addEventListener("keyup", (event) => {
+  playerYvelocity = 0;
+  playerXvelocity = 0;
+});
 
 function animate() {
   playerY += playerYvelocity;
